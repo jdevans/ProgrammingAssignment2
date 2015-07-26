4755c56bf0e9fe12cb1c543fa85f04e1c082bb88
John D. Evans <john.evans@gst.com>
2015-07-26 19:17:00 -0400

    Additional comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,20 @@
+## This pair of functions (makeCacheMatrix and cacheSolve) allow
+## for the result of a matrix inversion (a potentially lengthy computation)
+## to be cached and only recomputed if necessary. 
+##
+## Example usage:
+## > library(datasets)
+## > data(mtcars)
+## > m<-makeCacheMatrix(mtcars[1:6,1:6])
+## > cacheSolve(m)
+## Mazda RX4 Mazda RX4 Wag  Datsun 710 Hornet 4 Drive Hornet Sportabout     Valiant
+## mpg    1.6912172    -2.1246087  0.28373377    -0.35946411      -0.073188537  0.70128447
+## cyl    4.4779580    -4.5193481 -0.13701853    -0.88840025      -0.277132065  1.55731209
+## disp  -0.0959297     0.1149989 -0.01688931     0.04206857       0.002364724 -0.05303118
+## hp     0.1097871    -0.1599798  0.04452973    -0.07290840       0.016894914  0.07088814
+## drat -12.2658012    15.5529033 -1.88007166     3.63284633       0.246911606 -5.99578277
+## wt    -3.9215686     3.9215686  0.00000000     0.00000000       0.000000000  0.00000000
+
 
 ## The first function, makeCacheMatrix, creates a "pseudo-matrix", 
 ## which is really a list containing the following four functions:
